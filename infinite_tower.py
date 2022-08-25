fc98ec6869d65ea3fc2078e18a47a5147756fa9f
elite3312 <perrywu12@gmail.com>
2022-08-25 11:55:44 +0800

    added infinite tower simulation

---
+++ infinite_tower.py
@@ -0,0 +1,10 @@
+
+from this import d
+
+import numpy
+depth=18
+base_raised=power=constant_base=numpy.sqrt(2)
+for i in range(depth):
+    print(base_raised)
+    base_raised=numpy.power(constant_base,power)
+    power=base_raised
